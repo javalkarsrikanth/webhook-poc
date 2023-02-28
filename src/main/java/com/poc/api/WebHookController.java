da021e532dbe64f5ba31acefcc3d51c634d0a738
Srikanth <srikanthjavalkar@gmail.com>
2023-02-28 21:45:50 +0530

    Added location endpoint

--- src/main/java/com/poc/api/WebHookController.java
+++ src/main/java/com/poc/api/WebHookController.java
@@ -1,15 +1,18 @@
 package com.poc.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Webhook")
+@RequestMapping("/webhook")
 public class WebHookController {
 
     @GetMapping("/response")
     public String getResponse(){
         return "Hello Dear :) How are you ?";
+    }
+
+    @GetMapping("/location")
+    public String getLocation(){
+        return "India";
     }
 }
